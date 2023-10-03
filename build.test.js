b2e7262b7d30fcc36c1bcf69b2ef62579b7a34ab
Ivan Stankov <istankov+github-golang@gmail.com>
2023-10-03 13:58:56 +0300

    add dashboard metrics

--- build.test.js
+++ build.test.js
@@ -11,16 +11,17 @@ function runTest(dashboard) {
         },
     }
     
-    const dashboards = ['General'];
-    for (const dashboard of dashboards) {
-        builder.build(dashboard, options);
+    builder.build(dashboard, options);
 
-        const expected = JSON.parse(fs.readFileSync(`./testdata/dashboards/${dashboard}.json`, { encoding: 'utf8', flag: 'r' }));
-        const actual = JSON.parse(fs.readFileSync(`./build/dashboards/${dashboard}.json`, { encoding: 'utf8', flag: 'r' }));
-        expect(actual).toMatchObject(expected);
-    }
+    const expected = JSON.parse(fs.readFileSync(`./testdata/dashboards/${dashboard}.json`, { encoding: 'utf8', flag: 'r' }));
+    const actual = JSON.parse(fs.readFileSync(`./build/dashboards/${dashboard}.json`, { encoding: 'utf8', flag: 'r' }));
+    expect(actual).toMatchObject(expected);
 }
 
 test('build General dashboard should be as expected', () => {
     runTest('General')
+});
+
+test('build Metrics dashboard should be as expected', () => {
+    runTest('Metrics')
 });
